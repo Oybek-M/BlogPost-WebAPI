f7cdf43ebb2ed858824ac160a38c194a05eead6d
Oybek2007 <95219477+Oybek2007@users.noreply.github.com>
2024-04-24 21:28:54 +0500

    for test and first commit in Home-PC

---
+++ BlogPost.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost.Data.DbContexts
+{
+    internal class AppDbContext
+    {
+        // Test uchun, qolganini ertaga(from 24.04.2024 | 21:21)
+    }
+}
